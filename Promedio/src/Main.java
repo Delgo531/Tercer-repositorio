a132ca579470e950331e6b15be2f9b968090a92d
Diego <20243ds045@utez.edu.mx>
2025-05-21 08:18:57 -0600

    Finalizacion del programa

--- Promedio/src/Main.java
+++ Promedio/src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int numero1 =Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer digito: "));
+        int numero2 =Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo digito: "));
+        int numero3 =Integer.parseInt(JOptionPane.showInputDialog("Ingrese el tercer digito: "));
+        int resultado = (numero1 + numero2 + numero3) / 3;
+        JOptionPane.showMessageDialog(null, resultado);
+
     }
 }
